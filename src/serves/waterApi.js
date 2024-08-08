e0a0ac947fe61a4cdbed7c1376a6902a33431f71
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-08-08 17:40:06 +0100

    Update waterApi.js

--- src/serves/waterApi.js
+++ src/serves/waterApi.js
@@ -3,7 +3,7 @@ import { instance } from './authApi';
 export const dailyRecord = async (query) => {
     try{
         const params = { day: query };
-        const { data } = await instance.get("api/track/day", { params });
+        const { data } = await instance.get("/api/track/day", { params });
         console.log(data)
         return data
     } catch (error) {
@@ -13,14 +13,14 @@ export const dailyRecord = async (query) => {
 
 export const monthlyRecord = async (query) => {
     const params = { month: query };
-    const { data } = await instance.get("api/track/month", { params });
+    const { data } = await instance.get("/api/track/month", { params });
     console.log(data)
     return data;
 };
 
 export const addWaterRecord = async (body) => {
     try{
-        const {data} = await instance.post("api/track", body);
+        const {data} = await instance.post("/api/track", body);
         return data;
     } catch (error) {
         throw new Error(error.response?.data?.message || error.message || 'An error occurred');
@@ -28,11 +28,11 @@ export const addWaterRecord = async (body) => {
 };
 
 export const updateWaterRecord = async (id, amount, time) => {
-    const { data } = await instance.put(`api/track/${id}`, { amount, time });
+    const { data } = await instance.put(`/api/track/${id}`, { amount, time });
     return data;
 };
 
 export const deleteWaterRecord = async (id) => {
-    const { data } = await instance.delete(`api/track/${id}`);
+    const { data } = await instance.delete(`/api/track/${id}`);
     return data;
 };
